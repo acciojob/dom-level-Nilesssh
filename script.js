169664309a4fd064b3f9e2f02914726be11b5659
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-25 13:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //your JS code here. If required.
 const lis = document.querySelectorAll("li");
-
-for (let i = 0; i < lis.length; i++) {
-	if(lis[i].id==="level"){
-		alert(`The level of the element is: ${i+1}`)
+lis.forEach((value,index)=>{
+	if(value.id==="level"){
+		alert(`The level of the element is: ${index+1}`);
 	}
-}
+})
